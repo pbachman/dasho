efcf5ce620bcf3fc0de9cd92fb2bb3c63403594f
Phil B <hi@phil.ch>
2019-05-12 13:32:29 +0200

    Fixed Server Unittests, added missing authorise logic.

--- server/server.test.spec.js
+++ server/server.test.spec.js
@@ -10,21 +10,30 @@ describe('Dash0 backend/middleware', function () {
   let server;
   let token;
 
-  // TODO
-  // fix all Unittests with oauthServer.authorise().
-  before(function () {
+  before(function (done) {
     server = require('./index');
+
+    request(server)
+      .post('/oauth/token')
+      .send({
+        grant_type: 'password',
+        client_id: 'dasho',
+        client_secret: '$ecret',
+        username: 'hi@dasho.co',
+        password: 'test1234'
+      })
+      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .end(function (err, res) {
+        if (err) {
+          return done(err);
+        }
+        token = res.body.access_token;
+        done();
+      });
   });
 
   afterEach(function () {
     server.close();
-  });
-
-  it('InviteFriend_WithMissingUser_ShouldFailWith400', function method(done) {
-    request(server)
-      .post('/api/invite/')
-      .set('Authorization', 'Bearer ' + token)
-      .expect(400, done);
   });
 
   it('Login_WithMissingCredentials_ShouldFailWith400', function method(done) {
@@ -47,17 +56,10 @@ describe('Dash0 backend/middleware', function () {
       .expect(200, done);
   });
 
-  it('PwdReset_WithWrongUser_ShouldFailWith200', function method(done) {
+  it('PwdReset_WithWrongUser_ShouldFailWith400', function method(done) {
     request(server)
       .post('/api/pwdreset')
       .send({ username: 'wrong@user.com' })
-      .expect(200, done);
-  });
-
-  it('InviteFriend_WithMissingUser_ShouldFailWith400', function method(done) {
-    request(server)
-      .post('/api/invite/')
-      .set('Authorization', 'Bearer ' + token)
       .expect(400, done);
   });
 
@@ -77,12 +79,12 @@ describe('Dash0 backend/middleware', function () {
       .expect(400, done);
   });
 
-  it('PwdChange_WithWrongPassword_ShouldFailWith401', function method(done) {
+  it('PwdChange_WithWrongPassword_ShouldFailWith400', function method(done) {
     request(server)
       .put('/api/changepassword/')
       .set('Authorization', 'Bearer ' + token)
       .send({ username: 'hi@dasho.co', password: 'wrong', newpassword: 'wrong', newpasswordconfirm: 'wrong' })
-      .expect(401, done);
+      .expect(400, done);
   });
 
   it('PwdChange_WithWrongNewPassword_ShouldFailWith400', function method(done) {
@@ -103,6 +105,36 @@ describe('Dash0 backend/middleware', function () {
   it('UserSettings_WithCorrectUser_ShouldGet200', function method(done) {
     request(server)
       .get('/api/settings/hi@dasho.co')
+      .set('Authorization', 'Bearer ' + token)
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+
+  it('UserSettings_WithWrongUser_ShouldGet400', function method(done) {
+    request(server)
+      .get('/api/settings/wrong@user.com')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(400, done);
+  });
+
+  it('UserSettings_WithCorrectUser_ShouldGet200', function method(done) {
+    request(server)
+      .get('/api/settings/hi@dasho.co')
+      .set('Authorization', 'Bearer ' + token)
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+
+  it('UserUnassignedSettings_WithWrongUser_ShouldGet400', function method(done) {
+    request(server)
+      .get('/api/settings/unassigned/wrong@user.com')
+      .set('Authorization', 'Bearer ' + token)
+      .expect(400, done);
+  });
+
+  it('UserUnassignedSettings_WithCorrectUser_ShouldGet200', function method(done) {
+    request(server)
+      .get('/api/settings/unassigned/hi@dasho.co')
       .set('Authorization', 'Bearer ' + token)
       .expect('Content-Type', /json/)
       .expect(200, done);
